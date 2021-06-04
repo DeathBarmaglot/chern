3284a2e49219564ce8487282ef5704cefac6027b
Pasha <54247708+DeathBarmaglot@users.noreply.github.com>
2021-06-04 15:31:23 +0300

    Add files via upload

---
+++ index_edge.js
@@ -0,0 +1,282 @@
+/**
+ * Adobe Edge: symbol definitions
+ */
+(function($, Edge, compId){
+//images folder
+var im='images/';
+
+var fonts = {};
+var opts = {
+    'gAudioPreloadPreference': 'auto',
+
+    'gVideoPreloadPreference': 'auto'
+};
+var resources = [
+];
+var symbols = {
+"stage": {
+    version: "4.0.0",
+    minimumCompatibleVersion: "4.0.0",
+    build: "4.0.0.359",
+    baseState: "Base State",
+    scaleToFit: "none",
+    centerStage: "none",
+    initialState: "Base State",
+    gpuAccelerate: false,
+    resizeInstances: false,
+    content: {
+            dom: [
+            {
+                id: 'back',
+                type: 'image',
+                rect: ['0px', '-12px','312px','221px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"back.png",'0px','0px']
+            },
+            {
+                id: 'Magical-Winter-photo-overlay-22_copy_22',
+                type: 'image',
+                rect: ['0px', '-21px','312px','222px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"Magical-Winter-photo-overlay-22%20copy%202.png",'0px','0px']
+            },
+            {
+                id: 'Magical-Winter-photo-overlay-22_copy2',
+                type: 'image',
+                rect: ['2px', '-20px','312px','222px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"Magical-Winter-photo-overlay-22%20copy.png",'0px','0px']
+            },
+            {
+                id: 'stars',
+                type: 'image',
+                rect: ['21px', '14px','197px','161px','auto', 'auto'],
+                opacity: 0.75,
+                fill: ["rgba(0,0,0,0)",im+"stars.png",'0px','0px']
+            },
+            {
+                id: 'Untitled-2',
+                type: 'image',
+                rect: ['16px', '3px','238px','187px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"Untitled-2.png",'0px','0px'],
+                transform: [[],[],[],[],['36.8386%','38.1002%']]
+            },
+            {
+                id: 'photo',
+                type: 'image',
+                rect: ['0px', '3px','221px','179px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"photo.png",'0px','0px']
+            },
+            {
+                id: 'date',
+                type: 'image',
+                rect: ['5px', '-3px','48px','56px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"date.png",'0px','0px']
+            },
+            {
+                id: 'time',
+                type: 'image',
+                rect: ['-1px', '-3px','89px','103px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"time.png",'0px','0px']
+            },
+            {
+                id: 'Untitled-3',
+                type: 'image',
+                rect: ['2px', '-14px','312px','221px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"Untitled-3.png",'0px','0px']
+            },
+            {
+                id: 'Vector_Smart_Object-1',
+                type: 'image',
+                rect: ['-6px', '138px','312px','49px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"Vector%20Smart%20Object-1.png",'0px','0px']
+            },
+            {
+                id: 'Opticom',
+                type: 'image',
+                rect: ['190px', '6px','101px','42px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"Opticom.png",'0px','0px']
+            },
+            {
+                id: 'LOGO',
+                type: 'image',
+                rect: ['171px', '34px','142px','139px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"LOGO.png",'0px','0px']
+            },
+            {
+                id: 'Vector_Smart_Object-2',
+                type: 'image',
+                rect: ['236px', '133px','41px','41px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"Vector%20Smart%20Object-2.png",'0px','0px']
+            },
+            {
+                id: 'lenovo',
+                type: 'image',
+                rect: ['196px', '97px','130px','115px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"lenovo.png",'0px','0px']
+            },
+            {
+                id: 'Image',
+                type: 'image',
+                rect: ['33px', '140px','226px','48px','auto', 'auto'],
+                fill: ["rgba(0,0,0,0)",im+"%D0%A0%D0%9E%D0%97%D0%86%D0%93%D0%A0%D0%90%D0%A8%D0%86%20%D0%9F%D0%A0%D0%98%D0%97%D0%86%D0%92%20%D0%A1%D0%95%D0%A0%D0%95%D0%94%20%D0%93%D0%9B%D0%AF%D0%94%D0%90%D0%A7%D0%86%D0%92.png",'0px','0px']
+            }],
+            symbolInstances: [
+
+            ]
+        },
+    states: {
+        "Base State": {
+            "${_Untitled-3}": [
+                ["style", "top", '-14px'],
+                ["style", "height", '221px'],
+                ["style", "opacity", '0'],
+                ["style", "left", '2px'],
+                ["style", "width", '312px']
+            ],
+            "${_Magical-Winter-photo-overlay-22_copy2}": [
+                ["style", "top", '-20px'],
+                ["style", "opacity", '0'],
+                ["style", "left", '2px'],
+                ["transform", "rotateZ", '0deg']
+            ],
+            "${_time}": [
+                ["style", "top", '-3px'],
+                ["style", "background-position", [0,57.599853515625], {valueTemplate:'@@0@@px @@1@@px'} ],
+                ["style", "opacity", '0'],
+                ["style", "clip", [0,89,52.60009765625,0], {valueTemplate:'rect(@@0@@px @@1@@px @@2@@px @@3@@px)'} ],
+                ["style", "left", '-1px']
+            ],
+            "${_Magical-Winter-photo-overlay-22_copy_22}": [
+                ["style", "top", '-24px'],
+                ["style", "opacity", '0'],
+                ["style", "left", '-3px']
+            ],
+            "${_lenovo}": [
+                ["style", "top", '97px'],
+                ["style", "opacity", '0'],
+                ["style", "left", '196px']
+            ],
+            "${_stars}": [
+                ["style", "top", '14px'],
+                ["style", "opacity", '0'],
+                ["style", "left", '21px']
+            ],
+            "${_photo}": [
+                ["style", "top", '3px'],
+                ["transform", "scaleY", '0'],
+                ["style", "left", '0px'],
+                ["transform", "scaleX", '0']
+            ],
+            "${_Image}": [
+                ["style", "top", '140px'],
+                ["style", "opacity", '0'],
+                ["style", "left", '33px'],
+                ["style", "background-position", [-212,0], {valueTemplate:'@@0@@px @@1@@px'} ]
+            ],
+            "${_Vector_Smart_Object-2}": [
+                ["style", "top", '133px'],
+                ["style", "opacity", '0'],
+                ["style", "left", '236px']
+            ],
+            "${_Opticom}": [
+                ["style", "top", '6px'],
+                ["style", "opacity", '0'],
+                ["style", "left", '190px']
+            ],
+            "${_Untitled-2}": [
+                ["style", "top", '3px'],
+                ["style", "-webkit-transform-origin", [36.84,38.1], {valueTemplate:'@@0@@% @@1@@%'} ],
+                ["style", "-moz-transform-origin", [36.84,38.1],{valueTemplate:'@@0@@% @@1@@%'}],
+                ["style", "-ms-transform-origin", [36.84,38.1],{valueTemplate:'@@0@@% @@1@@%'}],
+                ["style", "msTransformOrigin", [36.84,38.1],{valueTemplate:'@@0@@% @@1@@%'}],
+                ["style", "-o-transform-origin", [36.84,38.1],{valueTemplate:'@@0@@% @@1@@%'}],
+                ["transform", "rotateZ", '0deg'],
+                ["style", "height", '187px'],
+                ["style", "opacity", '0'],
+                ["style", "left", '16px'],
+                ["style", "width", '238px']
+            ],
+            "${_back}": [
+                ["style", "top", '-12px'],
+                ["style", "opacity", '0'],
+                ["style", "left", '0px']
+            ],
+            "${_Vector_Smart_Object-1}": [
+                ["style", "top", '138px'],
+                ["transform", "scaleY", '1'],
+                ["transform", "scaleX", '1'],
+                ["style", "opacity", '0'],
+                ["style", "background-position", [343.2001953125,0], {valueTemplate:'@@0@@px @@1@@px'} ],
+                ["style", "left", '-6px']
+            ],
+            "${_date}": [
+                ["style", "top", '-3px'],
+                ["style", "height", '56px'],
+                ["style", "opacity", '0'],
+                ["style", "left", '5px'],
+                ["style", "width", '48px']
+            ],
+            "${_Stage}": [
+                ["color", "background-color", 'rgba(0,0,0,1.00)'],
+                ["style", "width", '300px'],
+                ["style", "height", '180px'],
+                ["style", "overflow", 'hidden']
+            ],
+            "${_LOGO}": [
+                ["style", "top", '34px'],
+                ["transform", "scaleY", '1'],
+                ["transform", "scaleX", '1'],
+                ["style", "clip", [0,142,83.800048828125,0], {valueTemplate:'rect(@@0@@px @@1@@px @@2@@px @@3@@px)'} ],
+                ["style", "background-position", [0,146.39990234375], {valueTemplate:'@@0@@px @@1@@px'} ],
+                ["style", "left", '171px']
+            ]
+        }
+    },
+    timelines: {
+        "Default Timeline": {
+            fromState: "Base State",
+            toState: "",
+            duration: 11000,
+            autoPlay: true,
+            timeline: [
+                { id: "eid3", tween: [ "transform", "${_photo}", "scaleX", '1', { fromValue: '0'}], position: 0, duration: 1250, easing: "easeInOutElastic" },
+                { id: "eid77", tween: [ "style", "${_date}", "opacity", '1', { fromValue: '0'}], position: 901, duration: 818, easing: "easeInOutElastic" },
+                { id: "eid95", tween: [ "style", "${_back}", "opacity", '0.75', { fromValue: '0'}], position: 0, duration: 590, easing: "easeInOutElastic" },
+                { id: "eid16", tween: [ "style", "${_Magical-Winter-photo-overlay-22_copy2}", "opacity", '0.75', { fromValue: '0'}], position: 282, duration: 1218, easing: "easeInOutElastic" },
+                { id: "eid92", tween: [ "style", "${_Vector_Smart_Object-1}", "background-position", [0,0], { valueTemplate: '@@0@@px @@1@@px', fromValue: [343.2001953125,0]}], position: 2435, duration: 609, easing: "easeInOutElastic" },
+                { id: "eid18", tween: [ "transform", "${_LOGO}", "scaleY", '0.9', { fromValue: '1'}], position: 1218, duration: 959, easing: "easeInOutElastic" },
+                { id: "eid101", tween: [ "style", "${_Image}", "background-position", [0,0], { valueTemplate: '@@0@@px @@1@@px', fromValue: [-212,0]}], position: 4375, duration: 253, easing: "easeInOutElastic" },
+                { id: "eid81", tween: [ "style", "${_time}", "background-position", [0,0], { valueTemplate: '@@0@@px @@1@@px', fromValue: [0,57.599853515625]}], position: 1078, duration: 642, easing: "easeInOutElastic" },
+                { id: "eid76", tween: [ "style", "${_time}", "opacity", '1', { fromValue: '0'}], position: 901, duration: 818, easing: "easeInOutElastic" },
+                { id: "eid51", tween: [ "style", "${_Untitled-2}", "opacity", '1', { fromValue: '0'}], position: 853, duration: 731, easing: "easeInOutElastic" },
+                { id: "eid68", tween: [ "style", "${_Opticom}", "opacity", '1', { fromValue: '0'}], position: 2084, duration: 896, easing: "easeInOutElastic" },
+                { id: "eid14", tween: [ "style", "${_Magical-Winter-photo-overlay-22_copy_22}", "opacity", '0.75', { fromValue: '0'}], position: 282, duration: 1218, easing: "easeInOutElastic" },
+                { id: "eid49", tween: [ "style", "${_Untitled-3}", "opacity", '1', { fromValue: '0'}], position: 683, duration: 731, easing: "easeInOutElastic" },
+                { id: "eid82", tween: [ "style", "${_time}", "clip", [0,89,103,0], { valueTemplate: 'rect(@@0@@px @@1@@px @@2@@px @@3@@px)', fromValue: [0,89,52.60009765625,0]}], position: 1078, duration: 642, easing: "easeInOutElastic" },
+                { id: "eid102", tween: [ "transform", "${_Untitled-2}", "rotateZ", '360deg', { fromValue: '0deg'}], position: 750, duration: 770, easing: "easeInOutElastic" },
+                { id: "eid42", tween: [ "style", "${_stars}", "opacity", '0.75', { fromValue: '0'}], position: 0, duration: 887, easing: "easeInOutElastic" },
+                { id: "eid44", tween: [ "style", "${_stars}", "opacity", '0', { fromValue: '0.75'}], position: 1000, duration: 770, easing: "easeInOutElastic" },
+                { id: "eid83", tween: [ "style", "${_stars}", "opacity", '0.75', { fromValue: '0'}], position: 1807, duration: 277, easing: "easeInOutElastic" },
+                { id: "eid84", tween: [ "style", "${_stars}", "opacity", '0', { fromValue: '0.75'}], position: 2084, duration: 770, easing: "easeInOutElastic" },
+                { id: "eid4", tween: [ "transform", "${_photo}", "scaleY", '1', { fromValue: '0'}], position: 0, duration: 1250, easing: "easeInOutElastic" },
+                { id: "eid22", tween: [ "style", "${_lenovo}", "opacity", '1', { fromValue: '0'}], position: 5500, duration: 443, easing: "easeInOutElastic" },
+                { id: "eid24", tween: [ "style", "${_Vector_Smart_Object-1}", "opacity", '1', { fromValue: '0'}], position: 2522, duration: 362, easing: "easeInOutElastic" },
+                { id: "eid10", tween: [ "style", "${_LOGO}", "background-position", [0,0], { valueTemplate: '@@0@@px @@1@@px', fromValue: [0,146.39990234375]}], position: 0, duration: 2435, easing: "easeInOutElastic" },
+                { id: "eid66", tween: [ "style", "${_Vector_Smart_Object-2}", "opacity", '1', { fromValue: '0'}], position: 2989, duration: 347, easing: "easeInOutElastic" },
+                { id: "eid94", tween: [ "style", "${_Vector_Smart_Object-2}", "opacity", '0', { fromValue: '1'}], position: 5403, duration: 347, easing: "easeInOutElastic" },
+                { id: "eid12", tween: [ "style", "${_LOGO}", "clip", [0,142,139,0], { valueTemplate: 'rect(@@0@@px @@1@@px @@2@@px @@3@@px)', fromValue: [0,142,83.800048828125,0]}], position: 0, duration: 2435, easing: "easeInOutElastic" },
+                { id: "eid17", tween: [ "transform", "${_LOGO}", "scaleX", '0.9', { fromValue: '1'}], position: 1218, duration: 959, easing: "easeInOutElastic" },
+                { id: "eid100", tween: [ "style", "${_Image}", "opacity", '1', { fromValue: '0'}], position: 4126, duration: 427, easing: "easeInOutElastic" }            ]
+        }
+    }
+}
+};
+
+
+Edge.registerCompositionDefn(compId, symbols, fonts, resources, opts);
+
+/**
+ * Adobe Edge DOM Ready Event Handler
+ */
+$(window).ready(function() {
+     Edge.launchComposition(compId);
+});
+})(jQuery, AdobeEdge, "EDGE-4750320");
